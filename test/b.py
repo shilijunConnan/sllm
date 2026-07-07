440b0119d654037bea26cad74e6323d5f9d11a32
shilijun <shilijun@cmss.chinamobile.com>
2026-07-07 11:26:17 +0800

    init v3

---
+++ test/b.py
@@ -0,0 +1,16 @@
+import torch
+from torch.utils.cpp_extension import load
+
+# 运行时自动编译
+my_lib = load(
+    name="my_add_extension",
+    sources=["a.cpp"],
+    verbose=True
+)
+
+# 使用编译后的扩展
+x = torch.ones(3)
+y = torch.ones(3) * 2
+z = my_lib.forward(x, y)
+
+print(z)  # 输出: tensor([3., 3., 3.])
